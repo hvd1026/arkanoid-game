1323ba3796a453bffc2894df6a4a9012940f4293
Phan Văn Phước Hưng <phanhungutral@gmail.com>
2025-10-06 15:41:33 +0700

    Add PowerUp subclasses and unit tests
    Introduced abstract PowerUp class with properties and methods, and added ExpandPaddlePowerUp and FastBallPowerUp subclasses. Created PowerUpTest to verify getter and setter functionality for PowerUp fields.

---
+++ src/main/java/objects/FastBallPowerUp.java
@@ -0,0 +1,29 @@
+package objects;
+
+public class FastBallPowerUp extends PowerUp{
+    public FastBallPowerUp(float x, float y, int width, int height) {
+        super(x, y, width, height);
+        setWidth(width);
+        setHeight(height);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void applyEffect(GameObject o) {
+
+    }
+
+    @Override
+    public void removeEffect(GameObject o) {
+
+    }
+}
