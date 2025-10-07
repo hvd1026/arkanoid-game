b22bee6bfe958eb236b31467997ac38b3c3b2162
Van Huan Dao <hvd1026@gmail.com>
2025-10-07 21:36:49 +0700

    feat: update test cases

---
+++ src/test/java/objects/PaddleTest.java
@@ -0,0 +1,38 @@
+package objects;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaddleTest {
+
+    @Test
+    @DisplayName("Test Paddle Creation")
+    void testPaddleCreation() {
+        Paddle paddle = new Paddle(50, 100, 200, 20);
+        assertNotNull(paddle);
+        assertEquals(50, paddle.getX());
+        assertEquals(100, paddle.getY());
+        assertEquals(200, paddle.getWidth());
+        assertEquals(20, paddle.getHeight());
+    }
+
+    @Test
+    @DisplayName("Test Paddle Movement 1")
+    void testPaddleMovement1() {
+        Paddle paddle = new Paddle(50, 100, 200, 20);
+        paddle.setDx(100); // move right 100px/s
+        paddle.update(1.0); // update for 1 second
+        assertEquals(150, paddle.getX()); // 50 + 100*1.0 = 150
+    }
+
+    @Test
+    @DisplayName("Test Paddle Movement 2")
+    void testPaddleMovement2() {
+        Paddle paddle = new Paddle((float) 50.5, 100, 200, 20);
+        paddle.setDx(100); // move right 100px/s
+        paddle.update(1 / 60.0); // update for 1 frame at 60fps
+        assertEquals(52.166666667, paddle.getX(), 0.0001); // 50.5 + 100*(1/60) = 52.166666667
+    }
+}
---
+++ src/test/java/objects/StrongBrickTest.java
@@ -0,0 +1,42 @@
+package objects;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StrongBrickTest {
+
+    StrongBrick brick;
+
+    @Test
+    @DisplayName("Test creation of StrongBrick")
+    void testCreation() {
+        brick = new StrongBrick(0, 0, 50, 20, (byte) 2, 10);
+        assertNotNull(brick);
+        assertEquals(10, brick.getHitPoints());
+        assertEquals((byte) 2, brick.getType());
+    }
+
+    @Test
+    @DisplayName("Test isDestroyed method")
+    void testIsDestroyed() {
+        brick = new StrongBrick(0, 0, 50, 20, (byte) 2, 2);
+        assertFalse(brick.isDestroyed(brick.getHitPoints()));
+        brick.takeHit();
+        assertFalse(brick.isDestroyed(brick.getHitPoints()));
+        brick.takeHit();
+        assertTrue(brick.isDestroyed(brick.getHitPoints()));
+    }
+
+    @Test
+    @DisplayName("Test takeHit method")
+    void testTakeHit() {
+        brick = new StrongBrick(0, 0, 50, 20, (byte) 2, 2);
+        assertEquals(2, brick.getHitPoints());
+        brick.takeHit();
+        assertEquals(1, brick.getHitPoints());
+        brick.takeHit();
+        assertEquals(0, brick.getHitPoints());
+    }
+}
