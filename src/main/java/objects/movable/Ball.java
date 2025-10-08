7e62ae9072b4e082ad31ac175d8a547bcc08afdc
Van Huan Dao <hvd1026@gmail.com>
2025-10-08 21:35:14 +0700

    refactor: category objects into package

--- src/main/java/objects/movable/Ball.java
+++ src/main/java/objects/movable/Ball.java
@@ -1,4 +1,4 @@
-package objects;
+package objects.movable;
 
 import java.awt.*;
 
--- src/test/java/objects/PaddleTest.java
+++ src/test/java/objects/PaddleTest.java
@@ -1,5 +1,6 @@
 package objects;
 
+import objects.movable.Paddle;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
