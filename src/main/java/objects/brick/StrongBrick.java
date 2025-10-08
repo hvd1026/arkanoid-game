3153e7bec24e7480d87dc644e71846723c81423a
Van Huan Dao <hvd1026@gmail.com>
2025-10-08 21:35:14 +0700

    refactor: category objects into package

--- src/main/java/objects/brick/StrongBrick.java
+++ src/main/java/objects/brick/StrongBrick.java
@@ -1,4 +1,4 @@
-package objects;
+package objects.brick;
 
 import java.awt.*;
 
--- src/test/java/objects/ExpandPaddlePowerUpTest.java
+++ src/test/java/objects/ExpandPaddlePowerUpTest.java
@@ -1,5 +1,6 @@
 package objects;
 
+import objects.powerup.ExpandPaddlePowerUp;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
