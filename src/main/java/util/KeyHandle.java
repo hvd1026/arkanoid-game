2b2c7273964d728cec4b5523ac6c9a6b2cac3f15
Van Huan Dao <hvd1026@gmail.com>
2025-10-10 20:19:15 +0700

    feat: key handle

---
+++ src/main/java/util/KeyHandle.java
@@ -0,0 +1,56 @@
+package util;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+public class KeyHandle implements KeyListener {
+    private static KeyHandle instance;
+    public boolean spacePressed, leftPressed, rightPressed;
+
+    private KeyHandle() {
+        spacePressed = false;
+        leftPressed = false;
+        rightPressed = false;
+    }
+
+    public static KeyHandle getInstance() {
+        if (instance == null) {
+            instance = new KeyHandle();
+        }
+        return instance;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_SPACE) {
+            spacePressed = true;
+        }
+        if (code == KeyEvent.VK_LEFT) {
+            leftPressed = true;
+        }
+        if (code == KeyEvent.VK_RIGHT) {
+            rightPressed = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_SPACE) {
+            spacePressed = false;
+        }
+        if (code == KeyEvent.VK_LEFT) {
+            leftPressed = false;
+        }
+        if (code == KeyEvent.VK_RIGHT) {
+            rightPressed = false;
+        }
+    }
+}
