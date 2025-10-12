80293932efc1aacaf827c2395bb6cb73ee46789e
Van Huan Dao <hvd1026@gmail.com>
2025-10-12 16:08:14 +0700

    feat: render paddle, paddle moving

--- src/main/java/objects/movable/Paddle.java
+++ src/main/java/objects/movable/Paddle.java
@@ -1,5 +1,8 @@
 package objects.movable;
 
+import util.Constant;
+import util.KeyHandle;
+
 import java.awt.*;
 
 public class Paddle extends MovableObject {
@@ -9,24 +12,37 @@ public class Paddle extends MovableObject {
 
     @Override
     public void update(double deltaTime) {
+        if (KeyHandle.getInstance().leftPressed) {
+            moveLeft();
+        } else if (KeyHandle.getInstance().rightPressed) {
+            moveRight();
+        } else {
+            setDx(0);
+        }
+
         move(deltaTime);
     }
 
     @Override
     public void render(Graphics2D g) {
-
+        g.setColor(Color.RED);
+        g.fillRect((int) getX(), (int) getY(), getWidth(), getHeight());
     }
 
     public void moveLeft() {
-//        setDx(-speed);
+        if (getX() <= 0) { // cham ben trai
+            setDx(0);
+            return;
+        }
+        setDx(-Constant.PADDLE_SPEED);
     }
 
     public void moveRight() {
-//        setDx(speed);
-    }
+        if (getX() + getWidth() >= Constant.SCREEN_WIDTH) { // cham ben phai
+            setDx(0);
+            return;
+        }
 
-    //Kiem tra va cham
-//    public boolean checkCollision(GameObject o) {
-//
-//    }
+        setDx(Constant.PADDLE_SPEED);
+    }
 }
