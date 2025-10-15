26999039fa10f0d3014555544e70c4e27e6ea22a
Van Huan Dao <hvd1026@gmail.com>
2025-10-15 20:31:14 +0700

    feat: NormalBrick can have many styles
    (cherry picked from commit 0f13692f60ab8430f55aa2ddb88c4b2b2cbcaebf)

--- src/main/java/objects/brick/NormalBrick.java
+++ src/main/java/objects/brick/NormalBrick.java
@@ -5,10 +5,18 @@ import util.AssetManager;
 import java.awt.*;
 
 public class NormalBrick extends Brick {
+    private int style;
 
     public NormalBrick(float x, float y, int width, int height) {
         super(x, y, width, height);
         setHitPoints(1); // toi da 1 lan cham.
+        this.style = 1; // mac dinh la style 1
+    }
+
+    public NormalBrick(float x, float y, int width, int height, int style) {
+        super(x, y, width, height);
+        setHitPoints(1); // toi da 1 lan cham.
+        this.style = style;
     }
 
     @Override
@@ -18,6 +26,6 @@ public class NormalBrick extends Brick {
 
     @Override
     public void render(Graphics2D g) {
-        AssetManager.getInstance().draw(g, util.Constant.NORMAL_BRICK_IMG_1, (int) getX(), (int) getY(), getWidth(), getHeight());
+        AssetManager.getInstance().draw(g, style, (int) getX(), (int) getY(), getWidth(), getHeight());
     }
 }
