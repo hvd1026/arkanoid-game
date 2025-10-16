f576b553575708aa44bbe8bc55552a5e45056482
Huan Dao Van <hvd1026@gmail.com>
2025-10-16 11:12:46 +0700

    fix: brick style
    (cherry picked from commit d7b90bd7d860c247a3e14b4d061fddcdd05bcf78)

--- src/main/java/util/AssetManager.java
+++ src/main/java/util/AssetManager.java
@@ -22,25 +22,25 @@ public class AssetManager {
                 g.drawImage(spriteSheet, x, y, x + width, y + height, 122, 32, 149, 57, null);
                 break;
             case Constant.NORMAL_BRICK_IMG_1:
-                g.drawImage(spriteSheet, x, y, x + width, y + height, 0, 0, 59, 30, null);
+                g.drawImage(spriteSheet, x, y, x + width, y + height, 0, 0, 58, 30, null);
                 break;
-//            case Constant.NORMAL_BRICK_IMG_2:
-//                g.drawImage(spriteSheet, x, y, x + width, y + height, 60, 0, 120, 30, null);
-//                break;
-//            case Constant.NORMAL_BRICK_IMG_3:
-//                g.drawImage(spriteSheet, x, y, x + width, y + height, 123, 0, 183, 30, null);
-//                break;
-//            case Constant.NORMAL_BRICK_IMG_4:
-//                g.drawImage(spriteSheet, x, y, x + width, y + height, 186, 0, 246, 30, null);
-//                break;
-//            case Constant.NORMAL_BRICK_IMG_5:
-//                g.drawImage(spriteSheet, x, y, x + width, y + height, 250, 0, 310 , 30, null);
-//                break;
-//            case Constant.NORMAL_BRICK_IMG_6:
-//                g.drawImage(spriteSheet, x, y, x + width, y + height, 313, 0, 373 , 30, null);
-//                break;
+            case Constant.NORMAL_BRICK_IMG_2:
+                g.drawImage(spriteSheet, x, y, x + width, y + height, 61, 0, 120, 30, null);
+                break;
+            case Constant.NORMAL_BRICK_IMG_3:
+                g.drawImage(spriteSheet, x, y, x + width, y + height, 123, 0, 182, 30, null);
+                break;
+            case Constant.NORMAL_BRICK_IMG_4:
+                g.drawImage(spriteSheet, x, y, x + width, y + height, 185, 0, 244, 30, null);
+                break;
+            case Constant.NORMAL_BRICK_IMG_5:
+                g.drawImage(spriteSheet, x, y, x + width, y + height, 247, 0, 306, 30, null);
+                break;
+            case Constant.NORMAL_BRICK_IMG_6:
+                g.drawImage(spriteSheet, x, y, x + width, y + height, 309, 0, 368, 30, null);
+                break;
             case Constant.STRONG_BRICK_IMG:
-                g.drawImage(spriteSheet, x, y, x + width, y + height, 370, 0, 430, 30, null);
+                g.drawImage(spriteSheet, x, y, x + width, y + height, 371, 0, 430, 30, null);
                 break;
             default:
                 System.out.println("Asset not found: " + id);
