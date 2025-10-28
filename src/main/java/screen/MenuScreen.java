d8912346fe535c699d59671e772c37927893fbc5
Van Huan Dao <hvd1026@gmail.com>
2025-10-29 00:13:43 +0700

    refactor(MenuScreen): remove unused import and write comments
    (cherry picked from commit b3a41f4a7f6b0e1073db47e07dbdb903117cdd4d)

--- src/main/java/screen/MenuScreen.java
+++ src/main/java/screen/MenuScreen.java
@@ -6,33 +6,41 @@ import util.AssetManager;
 import util.MouseHandle;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 import static util.Constant.SCREEN_HEIGHT;
 import static util.Constant.SCREEN_WIDTH;
 
-
+/**
+ * MenuScreen class is the main menu screen of the application.
+ * Contains a start button that allows users to begin the game.
+ */
 public class MenuScreen extends Screen {
-    private Button startButton;
+    private final Button startButton;
 
     public MenuScreen() {
-        FontMetrics fm = Toolkit.getDefaultToolkit().getFontMetrics(AssetManager.getInstance().getDefaultFont());
+        // Create a temporary image to get FontMetrics
+        BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = img.createGraphics();
+        g2d.setFont(AssetManager.getInstance().getDefaultFont());
+        FontMetrics fm = g2d.getFontMetrics();
+        // Calculate button size and position of the start button
         int startButtonWidth = fm.stringWidth("START");
         int startButtonHeight = fm.getHeight();
         int startButtonX = (SCREEN_WIDTH - startButtonWidth) / 2; // Centered horizontally
-        int startButtonY = (SCREEN_HEIGHT - startButtonHeight) * 2 / 3; // start at 2/3 of the screen height
+        int startButtonY = (SCREEN_HEIGHT - startButtonHeight) / 2; // Centered vertically
         startButton = new StartButton(startButtonX, startButtonY, startButtonWidth, startButtonHeight);
     }
 
     @Override
     public void update(double deltaTime) {
-
+        // Check mouse click on start button
         if (MouseHandle.getInstance().isClickOn(startButton)) {
-            System.out.println("Start Button Clicked!");
             MouseHandle.getInstance().changeToDefaultCursor();
             ScreenManager.getInstance().switchScreen(new LevelScreen());
             return;
         }
-
+        // Change cursor if hovering over start button
         if (MouseHandle.getInstance().isHoverOn(startButton)) {
             MouseHandle.getInstance().changeToHandCursor();
         } else {
