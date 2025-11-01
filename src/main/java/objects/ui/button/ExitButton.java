89c05b43582dc48f0ed79560e2badebe0f8c0949
minhhieu6th4 <lehieu06042006@gmail.com>
2025-11-01 23:05:46 +0700

    creat exitButton and load Logo

---
+++ src/main/java/objects/ui/button/ExitButton.java
@@ -0,0 +1,26 @@
+package objects.ui.button;
+
+import screen.LevelScreen;
+import screen.ScreenManager;
+import util.AssetManager;
+import util.MouseHandle;
+
+import java.awt.*;
+
+public class ExitButton extends Button {
+    public ExitButton(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public void update(double deltaTime) {
+
+    }
+
+    @Override
+    public void render(Graphics2D g) {
+        g.setFont(AssetManager.getInstance().getDefaultFont());
+        g.setColor(Color.WHITE);
+        g.drawString("EXIT", x, y + height);
+    }
+}
