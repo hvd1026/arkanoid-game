c3c758571b50e9b485e1c52cd5a185aab6e6a7d8
minhhieu6th4 <lehieu06042006@gmail.com>
2025-11-01 23:05:46 +0700

    creat exitButton and load Logo

--- src/main/java/screen/MenuScreen.java
+++ src/main/java/screen/MenuScreen.java
@@ -1,8 +1,10 @@
 package screen;
 
 import objects.ui.button.Button;
+import objects.ui.button.ExitButton;
 import objects.ui.button.StartButton;
 import util.AssetManager;
+import util.Constant;
 import util.MouseHandle;
 
 import java.awt.*;
@@ -17,19 +19,31 @@ import static util.Constant.SCREEN_WIDTH;
  */
 public class MenuScreen extends Screen {
     private final Button startButton;
+    private final Button exitButton;
 
     public MenuScreen() {
         // Create a temporary image to get FontMetrics
+
         BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2d = img.createGraphics();
         g2d.setFont(AssetManager.getInstance().getDefaultFont());
         FontMetrics fm = g2d.getFontMetrics();
+
         // Calculate button size and position of the start button
+
         int startButtonWidth = fm.stringWidth("START");
         int startButtonHeight = fm.getHeight();
         int startButtonX = (SCREEN_WIDTH - startButtonWidth) / 2; // Centered horizontally
         int startButtonY = (SCREEN_HEIGHT - startButtonHeight) / 2; // Centered vertically
         startButton = new StartButton(startButtonX, startButtonY, startButtonWidth, startButtonHeight);
+
+        // Calculate button size and position of the exit button
+
+        int exitButtonWidth = fm.stringWidth("EXIT");
+        int exitButtonHeight = fm.getHeight();
+        int exitButtonX = (SCREEN_WIDTH - exitButtonWidth) / 2; // Centered horizontally
+        int exitButtonY = ((SCREEN_HEIGHT - exitButtonHeight) / 2) + 50; // Centered vertically
+        exitButton = new ExitButton(exitButtonX, exitButtonY, exitButtonWidth, exitButtonHeight);
     }
 
     @Override
@@ -40,8 +54,16 @@ public class MenuScreen extends Screen {
             ScreenManager.getInstance().switchScreen(new LevelScreen());
             return;
         }
-        // Change cursor if hovering over start button
-        if (MouseHandle.getInstance().isHoverOn(startButton)) {
+
+        // Check mouse click on exit button
+        if (MouseHandle.getInstance().isClickOn(exitButton)) {
+            MouseHandle.getInstance().changeToDefaultCursor();
+            System.exit(0); // Thoát chương trình
+            return;
+        }
+
+        // Change cursor type based on hover state
+        if (MouseHandle.getInstance().isHoverOn(exitButton) || MouseHandle.getInstance().isHoverOn(startButton)) {
             MouseHandle.getInstance().changeToHandCursor();
         } else {
             MouseHandle.getInstance().changeToDefaultCursor();
@@ -51,6 +73,8 @@ public class MenuScreen extends Screen {
     @Override
     public void render(Graphics2D g) {
         AssetManager.getInstance().drawBackground(g);
+        AssetManager.getInstance().draw(g, Constant.LOGO_IMG, (SCREEN_WIDTH - 670) / 2, 60, 500, 156);
         startButton.render(g);
+        exitButton.render(g);
     }
 }
